3d1a6a382390caf33a4fbd5e49875475f1589e86
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-02-15 11:34:01 +0600

    cout color changed

--- script/clicked.js
+++ script/clicked.js
@@ -1,11 +1,22 @@
 function reset() {
     setValue(0);
+    setColor('green')
 }
 
 function increase() {
     const currentValue = getValueById('display');
     newValue = currentValue + 1;
     setValue(newValue);
+    if (newValue > 0) {
+        setColor('blue');
+    }
+    else if (newValue === 0) {
+        setColor('green');
+    }
+    else {
+        setColor('red');
+    }
+
 }
 
 
@@ -13,4 +24,18 @@ function decrease() {
     const currentValue = getValueById('display');
     newValue = currentValue - 1;
     setValue(newValue);
+    if (newValue > 0) {
+        setColor('blue');
+    }
+    else if (newValue === 0) {
+        setColor('green');
+    }
+    else {
+        setColor('red');
+    }
+}
+
+
+function setColor(color) {
+    document.getElementById('display').style.color = color;
 }
